3bf9196d455bd9d4adc7446771c2c9391cb0af78
Giordano de Brito <megagamesb@gmail.com>
2025-09-09 19:39:53 -0300

    Added more content types

--- src/Enums/ContentType.php
+++ src/Enums/ContentType.php
@@ -5,13 +5,22 @@ namespace GioPHP\Enums;
 class ContentType
 {
     public const Html = "text/html; charset=UTF-8";
+	public const CssText = "text/css";
+	public const JavaScript = "application/javascript";
 	public const PlainText = "text/plain";
 	public const Json = "application/json";
+	public const XML = "application/xml";
 	public const FileStream = "application/octet-stream";
 	public const ImageJpeg = "image/jpeg";
 	public const ImageJpg = "image/jpg";
 	public const ImagePng = "image/png";
 	public const ImageWebp = "image/webp";
+	public const ImageSvg = "image/svg+xml";
+	public const VideoMp4 = "video/mp4";
+	public const VideoWebm = "video/webm";
+	public const VideoOgg = "video/ogg";
+	public const PDF = "application/pdf";
+	public const Zip = "application/zip";
 }
 
 ?>
