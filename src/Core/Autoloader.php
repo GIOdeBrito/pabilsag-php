9278363ad204cca83c653f2ee22e726f909572ed
Giordano de Brito <megagamesb@gmail.com>
2025-09-14 13:27:54 -0300

    Autoloader namespace check

--- src/Core/Autoloader.php
+++ src/Core/Autoloader.php
@@ -13,9 +13,15 @@ spl_autoload_register(function (string $classname)
 {
 	// Get the root folder of the framework
 	$root = __DIR__.'/../';
+	$namespace = 'GioPHP';
 
 	// Splits the path to the selected class
 	$paths = explode('/', str_replace('\\', DIRECTORY_SEPARATOR, $classname));
+
+	if($paths[0] !== $namespace)
+	{
+		return;
+	}
 
 	// Removes the first item of the array
 	array_shift($paths);
